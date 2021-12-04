4817279bc3d786d38f51200a818a1f55165106bc
Thiago Moraes <thiagomoraesf95@gmail.com>
2021-12-03 22:09:24 -0300

    mais

---
+++ fundamentos_python/main.js.py
@@ -0,0 +1,9 @@
+nome = "Joao"
+idade = 25
+sobrenome = "Silva"
+
+print(nome, sobrenome, idade)
+
+print("Olá", nome, sobrenome, "sua idade é, ", idade)
+print(f"Olá, {nome} {sobrenome}, sua idade é, {idade}")
+
